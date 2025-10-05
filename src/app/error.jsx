432e59e96802c189ed7e7cab17ae5aab209e30f3
Divas <divas.verma1956@gmail.com>
2025-10-06 00:01:29 +0530

    Added global error.jsx and loading.jsx file

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,17 +1,16 @@
 'use client'
 
 export default function Error({ error, reset }) {
-
-  return ( // Use flex-grow to make the error component fill the available space
-  <div className="flex flex-col items-center justify-center h-full grow">
-    <h1 className="text-3xl font-bold text-red-500">Something Went Wrong</h1>
-    <p className="mt-4">Couldn't load the page. Please try again.</p>
-    <button
-      onClick={reset}
-      className="mt-6 bg-gray-600 hover:bg-gray-500 cursor-pointer text-white px-4 py-2 rounded"
-    >
-      Try Again
-    </button>
-  </div>
+  return (
+    <div className="flex flex-col items-center justify-center h-full grow">
+      <h2 className="text-2xl font-bold mb-4">Something went wrong!</h2>
+      <p className="text-red-500 mb-6">Failed to load data. Please try again later.</p>
+      <button
+        onClick={() => reset()}
+        className="bg-gray-600 hover:bg-gray-500 cursor-pointer text-white px-4 py-2 rounded"
+      >
+        Try again
+      </button>
+    </div>
   )
 }
