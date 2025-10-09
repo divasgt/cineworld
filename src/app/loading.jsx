930bd79e10e626a86355b1541a76dccf30211e0a
Divas <divas.verma1956@gmail.com>
2025-10-09 16:52:51 +0530

    Style: Change to shimmer animation on loading text

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className="flex justify-center items-center h-[calc(100vh-60px)]">
       {/* <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-red-700"></div> */}
-      <div className="animate-pulse  text-white text-2xl">Loading...</div>
+      <div className="shimmer text-white text-2xl">Loading...</div>
     </div>
   );
 }
