b4c5effdd599ee27cc161bd2d050a0f12bfd4f7d
Divas <divas.verma1956@gmail.com>
2025-10-12 01:51:00 +0530

    Updated RecommendationCard.jsx: Show star rating, and Links to search page when no tmdb data found for item

--- src/components/RecommendationCard.jsx
+++ src/components/RecommendationCard.jsx
@@ -12,11 +12,16 @@ export default function RecommendationCard({ item }) {
     ? `${IMAGE_BASE_URL}w342${item.tmdbData.poster_path}`
     : PLACEHOLDER_IMAGE_URL(placeholderWidth, placeholderHeight)
 
+  const rating = (hasTmdbData && item.tmdbData.vote_average) ? 
+    item.tmdbData.vote_average.toFixed(1)
+    : 'N/A'
+    
   const linkPath = hasTmdbData
     ? `/${item.media_type}/${item.tmdbData.id}`
-    : '#'
+    : `/search?q=${item.title}`
 
-  const CardContent = () => (
+  function CardContent() {
+    return (
     <div className={`flex gap-4 p-3 rounded-lg bg-gray-600/20 backdrop-blur-xl w-full max-w-2xl ${!hasTmdbData ? "opacity-70" : ""}`}>
       <div className="shrink-0">
         <Image
@@ -32,21 +37,23 @@ export default function RecommendationCard({ item }) {
         <h3 className="text-lg font-semibold" title={item.title}>
           {item.title} {item.release_year && `(${item.release_year})`}
         </h3>
-        <p className="text-sm text-gray-400 capitalize">
-          {item.type} {item.genres && `• ${item.genres.join(', ')}`}
-        </p>
+        <div className="text-sm text-gray-400 capitalize flex gap-2">
+          <span>{item.type}</span> • 
+          <span>{item.genres && `${item.genres.join(', ')}`}</span> • 
+          <span className="flex items-center gap-1 text-xs">
+            <span className="text-yellow-400 text-lg leading-4 -mt-0.5">★</span>
+            <span className="font-">{rating}</span>
+          </span>
+        </div>
         <p className="mt-2 text-sm text-gray-200">{item.reason}</p>
       </div>
     </div>
-  )
+    )
+  }
 
   return (
-    hasTmdbData ? (
-      <Link href={linkPath} className="transition-transform hover:scale-105 duration-300 ease-in-out hover:shadow-lg hover:shadow-black/40 backdrop-blur-xl shadow">
-        <CardContent />
-      </Link>
-    ) : (
+    <Link href={linkPath} className="transition-transform hover:scale-105 duration-300 ease-in-out hover:shadow-lg hover:shadow-black/40 backdrop-blur-xl shadow">
       <CardContent />
-    )
+    </Link>
   )
 }
