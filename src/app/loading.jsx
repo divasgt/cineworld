5ad065719ba60cd97da5cbbee60499a32d5aef70
Divas <divas.verma1956@gmail.com>
2025-10-15 18:53:05 +0530

    Style: Better style loading.jsx

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,8 +1,11 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-[calc(100vh-60px)]">
+    <div
+      // className="flex justify-center items-center h-[calc(100vh-60px)]"
+      className="my-auto -translate-y-20 text-center"
+    >
       {/* <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-red-700"></div> */}
-      <div className="shimmer-text text-white text-2xl">Loading...</div>
+      <span className="shimmer-text text-white text-2xl">Loading...</span>
     </div>
   );
 }
