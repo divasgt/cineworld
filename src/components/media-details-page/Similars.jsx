32a081ec4aaa48fc55784fdb525b462bb7426652
Divas <divas.verma1956@gmail.com>
2025-10-18 01:21:50 +0530

    Style: Better responsive design of Media Details page

--- src/components/media-details-page/Similars.jsx
+++ src/components/media-details-page/Similars.jsx
@@ -2,13 +2,10 @@ import MediaCard from "../MediaCard";
 
 export default function Similars({data, type}) {
   return (
-  // <div>
-  //   <h2 className='text-2xl mb-5 border-b-2 border-b-red-500 inline-block pb-2'>Top Cast</h2>
-    <div className='flex gap-5 overflow-x-auto p-4 -m-4'>
-      {data.map(item => (
-        <MediaCard key={item.id} item={item} isMovie={type==="movie"} layoutType="horizontal" />
-      ))}
-    </div>
-  // </div>
+  <>
+    {data.map(item => (
+      <MediaCard key={item.id} item={item} isMovie={type==="movie"} layoutType="horizontal" />
+    ))}
+  </>
   )
 }
--- src/components/media-details-page/WatchlistBtn.jsx
+++ src/components/media-details-page/WatchlistBtn.jsx
@@ -70,7 +70,7 @@ export default function WatchlistBtn({tmdbId, title, type, year, posterPath}) {
       // onMouseEnter={handleMouseEnter}
       // onMouseLeave={handleMouseLeave}
       disabled={loading}
-      className="inline-flex items-center justify-center px-4 py-2.5 text-sm font-semibold rounded-md cursor-pointer bg-gray-600/70 shadow-sm backdrop-blur-xl text-white border-none hover:bg-gray-600 transition-colors"
+      className="flex items-center justify-center px-4 py-3 text-sm font-semibold rounded-md cursor-pointer bg-gray-600/70 shadow-sm backdrop-blur-xl text-white border-none hover:bg-gray-600 transition-colors"
     >
       {isInWatchlist ? 
         // isButtonHovered ? 
