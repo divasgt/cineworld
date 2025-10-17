e9c8f8bec4f4510e95cb6f03d647b5f5e4d6918a
Divas <divas.verma1956@gmail.com>
2025-10-17 23:41:41 +0530

    Added AlertPopup.jsx; Used it in WatchlistBtn.jsx

---
+++ src/components/AlertPopup.jsx
@@ -0,0 +1,36 @@
+"use client"
+import { useEffect, useState } from "react"
+import { MdClose } from "react-icons/md"
+import { twMerge } from "tailwind-merge"
+
+export default function AlertPopup({message, duration=2000, className}) {
+  const [showAlert, setShowAlert] = useState(false)
+
+  useEffect(() => {
+    if (message) {
+      setShowAlert(true)
+
+      const timer = setTimeout(() => {
+        setShowAlert(false)
+      }, duration)
+      
+      return () => clearTimeout(timer)
+    }
+  }, [message, duration])
+
+  return (
+    <div
+      className={twMerge(
+        `fixed flex items-center top-20 right-5 z-20 px-4 py-2 bg-gray-700 backdrop-blur-xl text-white rounded-md border border-gray-400/10 shadow-black shadow-xl transition-all duration-300 ease-in-out ${showAlert ? 'opacity-100 scale-100' : 'opacity-0 scale-95 pointer-events-none'}`, className
+      )}
+    >
+      {message}
+      {/* <button 
+        onClick={() => setShowAlert(false)} 
+        className="ml-2 text-blue-400 cursor-pointer hover:bg-gray-600 rounded-md p-1 transition-colors"
+      >
+        <MdClose className='size-4'/>
+      </button> */}
+    </div>
+  )
+}
--- src/components/media-details-page/WatchlistBtn.jsx
+++ src/components/media-details-page/WatchlistBtn.jsx
@@ -3,62 +3,93 @@ import { useAuth } from '@/hooks/useAuth';
 import { useWatchlist } from '@/hooks/useWatchlist';
 import { useState } from 'react';
 import { MdBookmarkAdded, MdBookmarkRemove, MdOutlineBookmarkAdd } from 'react-icons/md';
+import AlertPopup from '@/components/AlertPopup';
+import Link from 'next/link';
 
 export default function WatchlistBtn({tmdbId, title, type, year, posterPath}) {
   tmdbId = Number(tmdbId)
   const {user} = useAuth()
   const {watchlist, addToWatchlist, removeFromWatchlist, loading} = useWatchlist(user?.id)
   const isInWatchlist = watchlist.some(item => item.tmdb_id===tmdbId)
-  const [isButtonHovered, setIsButtonHovered] = useState(false)
-  const [hoverTimeout, setHoverTimeout] = useState(null)
-  
+  // const [isButtonHovered, setIsButtonHovered] = useState(false)
+  // const [hoverTimeout, setHoverTimeout] = useState(null)
+  const [alertMessage, setAlertMessage] = useState('')
+
   const handleClick = async () => {
-    if (!user) return
+    if (!user) {
+      setAlertMessage(
+        <>
+          Please
+          <Link href="/signin" className="text-blue-400 hover:text-blue-300 cursor-pointer mx-1.5">Sign In</Link>
+          to use this feature
+        </>
+      )
+      return
+    }
     
     try {
-      if (isInWatchlist) await removeFromWatchlist(tmdbId)
-      else await addToWatchlist(tmdbId, title, type, year, posterPath)
+      if (isInWatchlist) {
+        await removeFromWatchlist(tmdbId)
+        setAlertMessage(
+          <>
+            Removed from 
+            <Link href="/watchlist" className="text-blue-400 hover:text-blue-300 cursor-pointer mx-1.5">Watchlist</Link>
+          </>
+        )
+      } else {
+        await addToWatchlist(tmdbId, title, type, year, posterPath)
+        // setAlertMessage(
+        //   <>
+        //     Added to 
+        //     <Link href="/watchlist" className="text-blue-400 hover:text-blue-300 cursor-pointer mx-1.5">Watchlist</Link>
+        //   </>
+        // )
+      }
     } catch(err) {
-      if (!isInWatchlist) alert('Failed to add to watchlist')
-      else alert('Failed to remove from watchlist')
+      setAlertMessage(isInWatchlist ? 'Failed to remove from watchlist' : 'Failed to add to watchlist')
     }
   }
 
-  const handleMouseEnter = () => {
-    if (loading) return;
-    if (hoverTimeout) clearTimeout(hoverTimeout); // Clear any existing timeout
-    setHoverTimeout(setTimeout(() => setIsButtonHovered(true), 200));
-  };
+  // const handleMouseEnter = () => {
+  //   if (loading) return
+  //   if (hoverTimeout) clearTimeout(hoverTimeout) // Clear any existing timeout
+  //   setHoverTimeout(setTimeout(() => setIsButtonHovered(true), 200))
+  // }
 
-  const handleMouseLeave = () => {
-    if (loading) return;
-    if (hoverTimeout) clearTimeout(hoverTimeout); // Clear any existing timeout
-    setHoverTimeout(setTimeout(() => setIsButtonHovered(false), 200));
-  };
-  
+  // const handleMouseLeave = () => {
+  //   if (loading) return
+  //   if (hoverTimeout) clearTimeout(hoverTimeout) // Clear any existing timeout
+  //   setHoverTimeout(setTimeout(() => setIsButtonHovered(false), 200))
+  // }
+
   return (
-  <button
-    id="watchlistBtn"
-    onClick={handleClick}
-    onMouseEnter={handleMouseEnter}
-    onMouseLeave={handleMouseLeave}
-    disabled={loading}
-    className="inline-flex items-center justify-center px-4 py-2.5 text-sm font-semibold rounded-md cursor-pointer bg-gray-600/70 shadow-sm backdrop-blur-xl text-white border-none hover:bg-gray-600 transition-colors"
-  >
-    {isInWatchlist ?
-      isButtonHovered ?
-      <>
-        <MdBookmarkRemove className="mr-2.5 size-5" />Remove from Watchlist
-      </>
+  <>
+    <button
+      id="watchlistBtn"
+      onClick={handleClick}
+      // onMouseEnter={handleMouseEnter}
+      // onMouseLeave={handleMouseLeave}
+      disabled={loading}
+      className="inline-flex items-center justify-center px-4 py-2.5 text-sm font-semibold rounded-md cursor-pointer bg-gray-600/70 shadow-sm backdrop-blur-xl text-white border-none hover:bg-gray-600 transition-colors"
+    >
+      {isInWatchlist ? 
+        // isButtonHovered ? 
+        // <>
+        //   <MdBookmarkRemove className="mr-2.5 size-5" />Remove from Watchlist
+        // </>
+        // :
+        <>
+          <MdBookmarkAdded className="mr-2.5 size-5" />Added to Watchlist
+        </>
       :
-      <>
-        <MdBookmarkAdded className="mr-2.5 size-5" />Added to Watchlist
-      </>
-    :
-      <>
-        <MdOutlineBookmarkAdd className="mr-2.5 size-5" />Add to Watchlist
-      </>
-    }
-  </button>
+        <>
+          <MdOutlineBookmarkAdd className="mr-2.5 size-5" />Add to Watchlist
+        </>
+      }
+    </button>
+
+    {/* Render the AlertPopup with message */}
+    <AlertPopup message={alertMessage} />
+  </>
   )
 }
