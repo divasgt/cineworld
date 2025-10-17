06485fdd484a02d32ea192f3aad27a552ab28b86
Divas <divas.verma1956@gmail.com>
2025-10-18 01:23:49 +0530

    Style: Better responsive design of home page

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -38,11 +38,11 @@ export default async function Home() {
 	}
 
 	return (
-	<main className="w-full mx-auto px-10 py-8 md:py-8">
+	<main className="w-full mx-auto px-5 md:px-10 py-8">
 
-		<section className="intro-section flex flex-col text-center rounded-xl py-20 px-8 mb-8" id="intro-section">
-			<h1 className="intro-title text-4xl font-bold text-white mb-4">Your One-Stop Website for All Things Cinema</h1>
-			<p className="intro-text text-xl text-[#9CA3AF] max-w-3xl mx-auto">
+		<section className="intro-section flex flex-col text-center rounded-xl py-10 md:py-20 md:px-8 mb-8" id="intro-section">
+			<h1 className="intro-title text-2xl md:text-4xl font-bold text-white mb-4">Your One-Stop Website for All Things Cinema</h1>
+			<p className="intro-text text-lg md:text-xl text-[#9CA3AF] max-w-3xl mx-auto">
 				Explore a vast collection of movies and TV shows,
 				from the latest blockbusters to timeless classics.
 			</p>
