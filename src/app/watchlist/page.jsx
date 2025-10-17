0cf8fb5f49ee05ca69fbd4c200fe087b52bf8410
Divas <divas.verma1956@gmail.com>
2025-10-17 21:52:15 +0530

    Add sign in link on watchlist page when not signed in

--- src/app/watchlist/page.jsx
+++ src/app/watchlist/page.jsx
@@ -5,13 +5,19 @@ import { useWatchlist } from '@/hooks/useWatchlist'
 import Loading from '../loading'
 import { MediaContainer } from '@/components/MediaContainer'
 import MediaCard from '@/components/MediaCard'
+import Link from 'next/link'
 
 export default function WatchlistPage() {
   const { user, loading: authLoading } = useAuth()
   const { watchlist, loading } = useWatchlist(user?.id)
 
   if (authLoading) return <Loading />
-  if (!user) return <div className='text-center mt-36 text-xl'>Please sign in to view your watchlist</div>
+  if (!user) return (
+    <div className='text-center mt-36 text-xl'>
+      Please
+      <Link href="/signin" className="text-blue-400 hover:text-blue-300 cursor-pointer mx-1.5">Sign In</Link>
+      to view your watchlist
+    </div>)
 
   return (
     <div className="w-full mx-auto px-10 py-8 md:py-8">
