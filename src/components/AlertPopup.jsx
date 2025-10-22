5a3ec6032925c3e09346103070947c53e9d3edc1
Divas <divas.verma1956@gmail.com>
2025-10-22 22:43:03 +0530

    Alert Popup: change default duration, made responsive design

--- src/components/AlertPopup.jsx
+++ src/components/AlertPopup.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 import { MdClose } from "react-icons/md"
 import { twMerge } from "tailwind-merge"
 
-export default function AlertPopup({message, duration=2000, className=""}) {
+export default function AlertPopup({message, duration=2500, className=""}) {
   const [showAlert, setShowAlert] = useState(false)
 
   useEffect(() => {
@@ -21,7 +21,7 @@ export default function AlertPopup({message, duration=2000, className=""}) {
   return (
     <div
       className={twMerge(
-        `fixed flex items-center top-20 right-5 z-20 px-4 py-2 bg-gray-700 backdrop-blur-xl text-white rounded-md border border-gray-400/10 shadow-black shadow-xl transition-all duration-300 ease-in-out ${showAlert ? 'opacity-100 scale-100' : 'opacity-0 scale-95 pointer-events-none'}`, className
+        `fixed flex items-center top-20 right-5 max-w-80 md:left-auto md:max-w-96 z-20 justify-center md:justify-start px-4 py-2 bg-gray-700 backdrop-blur-xl text-white text-xs md:text-base rounded-md border border-gray-400/10 shadow-black shadow-xl transition-all duration-300 ease-in-out ${showAlert ? 'opacity-100 scale-100' : 'opacity-0 scale-95 pointer-events-none'}`, className
       )}
     >
       {message}
