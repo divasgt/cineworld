cf294e34ccf487a62809eb7ae855318c80389caf
Divas <divas.verma1956@gmail.com>
2025-10-24 01:00:08 +0530

    Style: Enhance Alert Popup style

--- src/components/AlertPopup.jsx
+++ src/components/AlertPopup.jsx
@@ -21,16 +21,16 @@ export default function AlertPopup({message, duration=2500, className=""}) {
   return (
     <div
       className={twMerge(
-        `fixed flex items-center top-20 right-5 max-w-80 md:left-auto md:max-w-96 z-20 justify-center md:justify-start px-4 py-2 bg-gray-700 backdrop-blur-xl text-white text-xs md:text-base rounded-md border border-gray-400/10 shadow-black shadow-xl transition-all duration-300 ease-in-out ${showAlert ? 'opacity-100 scale-100' : 'opacity-0 scale-95 pointer-events-none'}`, className
+        `fixed flex items-center top-20 right-5 max-w-80 md:left-auto md:max-w-96 z-20 justify-center md:justify-start pl-4 pr-3 py-2 bg-gray-700 backdrop-blur-xl text-white text-sm md:text-base rounded-md border border-gray-400/10 shadow-black shadow-xl transition-all duration-300 ease-in-out ${showAlert ? 'opacity-100 scale-100' : 'opacity-0 scale-95 pointer-events-none'}`, className
       )}
     >
       {message}
-      {/* <button 
+      <button 
         onClick={() => setShowAlert(false)} 
-        className="ml-2 text-blue-400 cursor-pointer hover:bg-gray-600 rounded-md p-1 transition-colors"
+        className="ml-2 text-gray-400 cursor-pointer hover:text-white hover:bg-gray-600 rounded-md p-1 transition-colors"
       >
         <MdClose className='size-4'/>
-      </button> */}
+      </button>
     </div>
   )
 }
