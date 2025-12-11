c9c46488675e73f29ea8cdce61f51459a96fc02d
Divas <divas.verma1956@gmail.com>
2025-12-12 05:13:12 +0530

    update: Enhance MediaContainer with better spacing and sizes and show overflow card content in horizontal container

--- src/components/MediaContainer.jsx
+++ src/components/MediaContainer.jsx
@@ -3,13 +3,13 @@ export function MediaContainer({id="", type="grid-container", children}) {
   return (
   <>
     {type==="grid-container" && 
-      <div id={id} className="grid-container grid grid-cols-3 gap-4 pb-4 md:grid-cols-4 md:gap-4 lg:grid-cols-6 lg:gap-5">
+      <div id={id} className="grid-container grid grid-cols-3 gap-x-3 gap-y-5 pb-4 sm:gap-x-4 md:grid-cols-4 md:gap-x-5 md:gap-y-6 lg:grid-cols-5 xl:grid-cols-6">
         {children}
       </div>
     }
 
     {type==="horizontal-container" && 
-      <div id={id} className="horizontal-container flex overflow-x-auto gap-4 lg:gap-5 p-4 -m-4 *:shrink-0 md:*:w-52 *:w-40">
+      <div id={id} className="horizontal-container flex overflow-x-auto overflow-y-visible gap-2 sm:gap-4 md:gap-5 px-8 py-60 -mx-8 -my-60 *:shrink-0 *:w-36 md:*:w-52">
         {children}
       </div>
     }
