3b4d728f9815684afe0215a9e5773769bb00af52
Divas <divas.verma1956@gmail.com>
2025-12-12 05:46:37 +0530

    update: Add some margin top to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-  <footer className="bg-gray-900/30 backdrop-blur-xl border-t border-gray-500/10 text-gray-500 text-center p-4 mt-auto bottom-0 left-0 right-0">
+  <footer className="bg-gray-900/30 backdrop-blur-xl text-gray-500 text-center p-4 mt-48 bottom-0 left-0 right-0">
     <p className="whitespace-pre-wrap">© 2025 CineWorld.  All rights reserved.  Data provided by TMDB.</p>
   </footer>
   )
