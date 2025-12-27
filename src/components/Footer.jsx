21af854bc37aa9c0e1a7aee71280e5892aac3df6
Divas <divas.verma1956@gmail.com>
2025-12-27 23:01:03 +0530

    update: Update footer, also use new app name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,26 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
   <footer className="bg-gray-900/30 backdrop-blur-xl text-gray-500 text-center p-4 mt-48 bottom-0 left-0 right-0">
-    <p className="whitespace-pre-wrap">© 2025 CineWorld.  All rights reserved.  Data provided by TMDB.</p>
+    <div className="flex justify-between items-center px-10 pb-2">
+      <div className="flex flex-col items-start gap-3">
+        <span className="font-bold">Cinema World</span>
+        <nav>
+          <ul className="flex justify-center gap-4 *:hover:text-gray-300 *:duration-150">
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="#">About</Link></li>
+            <li><Link href="#">Terms</Link></li>
+            <li><Link href="#">Privacy</Link></li>
+            <li><Link href="#">Contact</Link></li>
+          </ul>
+        </nav>
+      </div>
+      <span className="whitespace-pre flex flex-col items-end gap-2">
+        <span>Data provided by TMDB.</span>
+        <span>Made with ❤️ in India</span>
+      </span>
+    </div>
   </footer>
   )
 }
